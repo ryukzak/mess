157d7ac4371cacb24479ac629ac411f96fb9a64e
Ryukzak Neskazov <ryukzak@gmail.com>
2010-03-17 18:05:04 +0300

    REF rfid_reader

--- src/rfid/rfid_reader.erl
+++ src/rfid/rfid_reader.erl
@@ -5,7 +5,6 @@
 				 , cast/2
 				 , terminate/1
 				 , enviroment/0
-				 , fib/1
 				 , init_bool/0
 				]).
 
@@ -21,84 +20,60 @@
 init_bool() -> true.
 
 enviroment() ->
-		{atomic, ok} = mnesia:create_table(sdk, [{attributes,record_info(fields, rfid_reader)}]),
-		{atomic, ok} = mnesia:create_table(data, [{attributes,record_info(fields, mark_info)}
-																							, {type, bag}
-																						 ]),
+		{atomic, ok} = mnesia:create_table(rfid_reader, [{attributes,record_info(fields, rfid_reader)}]),
+		{atomic, ok} = mnesia:create_table(mark_info, [{attributes,record_info(fields, mark_info)}
+																									 , {type, bag}
+																									]),
 		ok.
 
-
-
-init(["id", IDstr, Xstr, Ystr], _State) ->
-		ID = list_to_integer(IDstr),
-		X = list_to_integer(Xstr),
-		Y = list_to_integer(Ystr),
-		mnesia:transaction(fun() -> mnesia:write(#rfid_reader{id = ID,
+init(["id", IdS, Xs, Ys], _State) ->
+		Id = list_to_integer(IdS),
+    X = list_to_integer(Xs),
+    Y = list_to_integer(Ys),
+		mnesia:transaction(fun() -> mnesia:write(#rfid_reader{id = Id,
 																													pid = self()
 																												 }) end),
-		{false, #state{id = ID, x = X, y = Y}}.
+		{false, #state{id = Id, x = X, y = Y}}.
 
-
-
-tcp_cast(["here", MIDstr], #state{id = Id} = State) ->
-		MID = list_to_integer(MIDstr),
+tcp_cast(["here", MidS], #state{id=Id} = State) ->
+		Mid = list_to_integer(MidS),
 		mnesia:transaction(
-			fun() -> mnesia:write(#mark_info{mid = MID
+			fun() -> mnesia:write(#mark_info{mid = Mid
 																			 , id = Id
 																			}) end),
 		State;
 
-tcp_cast(["unhere", MIDstr], #state{id = Id} = State) ->
-		MID = list_to_integer(MIDstr),		
+tcp_cast(["unhere", MidS], #state{id=Id} = State) ->
+		Mid = list_to_integer(MidS),		
 		mnesia:transaction(
-			fun() -> mnesia:delete(#mark_info{mid = MID
+			fun() -> mnesia:delete(#mark_info{mid = Mid
 																				, id = Id
 																			 }) end),
 		State;
 
-tcp_cast(["radius", MIDStr, RadStr], #state{x=X, y=Y,
-																						where_request=WhereRequest
-																					 } = State) ->
-		MID = list_to_integer(MIDStr),
-		Rad = list_to_integer(RadStr),
-		Waits = wait_mid(WhereRequest, MID),
-		lists:map(fun(p) -> p ! {radius, MID, Rad, X, Y} end, Waits),
+tcp_cast(["radius", MidS, RadS], #state{x=X, y=Y, where_request=WhereRequest
+																			 } = State) ->
+		Mid = list_to_integer(MidS),
+		Rad = list_to_integer(RadS),
+		Waits = wait_mid(WhereRequest, Mid),
+		lists:map(fun(p) -> p ! {radius, Mid, Rad, X, Y} end, Waits),
 		State.
 
-
-
-cast({where, MID, Pid}, #state{where_request=WhereRequest} = State) ->
-		case wait_mid(WhereRequest, MID) of
+cast({where, Mid, Pid}, #state{where_request=WhereRequest} = State) ->
+		case wait_mid(WhereRequest, Mid) of
 				[] ->
-						Msg = "where " ++ erlang:integer_to_list(MID, 10) ++ "\n",
-						{Msg, State#state{where_request = [{MID, Pid} | WhereRequest]}};
+						Msg = "where " ++ erlang:integer_to_list(Mid, 10) ++ "\n",
+						{Msg, State#state{where_request = [{Mid, Pid} | WhereRequest]}};
 				_ ->
-						{none, State#state{where_request = [{MID, Pid} | WhereRequest]}}
+						{none, State#state{where_request = [{Mid, Pid} | WhereRequest]}}
 		end.
 
-wait_mid(WhereRequest, MID) ->
-		lists:filter(fun({M, _}) when M == MID -> true;
+wait_mid(WhereRequest, Mid) ->
+		lists:filter(fun({M, _}) when M == Mid -> true;
 										(_) -> false
 								 end, WhereRequest).
 
-		
-
-%% fib(1) -> 1;
-%% fib(2) -> 1;
-%% fib(N) ->
-%% 		fib(N, 2, 1, 1).
-
-%% fib(N, N, A, B) ->
-%% 		A + B;
-%% fib(N, C, A, B) ->
-%% 		fib(N, C+1, B, B + A).
-
-fib(0) -> 1;
-fib(1) -> 1;
-fib(2) -> 1;
-fib(N) -> fib(N - 1) + fib(N - 2).
-
-terminate(#state{id = Id}) ->
-		mnesia:transaction(fun() -> mnesia:delete({sdk, Id}) end),
+terminate(#state{id=Id}) ->
+		mnesia:transaction(fun() -> mnesia:delete({rfid_reader, Id}) end),
 		ok.
 
