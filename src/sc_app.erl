c05ed9f66f4ee7139f42ce1c497f27eee0936fff
Ryukzak Neskazov <ryukzak@gmail.com>
2010-03-22 17:37:17 +0300

    FIX sasl config

--- src/sc_app.erl
+++ src/sc_app.erl
@@ -35,12 +35,15 @@
 %%--------------------------------------------------------------------
 start(_StartType, _StartArgs) ->
 		application:start(mnesia),
+
+    file:make_dir("log"),
     
-    application:set_env(sasl, sasl_error_logger, {file, "simple_test_log"}),
-    application:set_env(sasl, errlog_type, error),
-    application:set_env(sasl, error_logger_mf_dir, "log"),
-    application:set_env(sasl, error_logger_mf_maxbytes, 10485760),
-    application:set_env(sasl, error_logger_mf_maxfiles, 10),
+%%     application:set_env(sasl, sasl_error_logger, {file, "simple_test_log"}),
+%%     application:set_env(sasl, sasl_error_logger, false),
+%%     application:set_env(sasl, errlog_type, error),
+%%     application:set_env(sasl, error_logger_mf_dir, "log"),
+%%     application:set_env(sasl, error_logger_mf_maxbytes, 10485760),
+%%     application:set_env(sasl, error_logger_mf_maxfiles, 10),
     application:start(sasl),
 
 		case node_sup:start_link() of
