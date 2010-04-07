6be90a6e9edeb9a9a775dec986cac9e9928bb50a
Ryukzak Neskazov <ryukzak@gmail.com>
2010-04-07 17:46:33 +0400

    Add normal switch master node

--- master_node.erl
+++ master_node.erl
@@ -12,75 +12,94 @@
 
 %% API
 -export([
-				 start_link/0
-				 , attach/1
-				 , ping/0
-				]).
+         start_link/0
+         , attach/1
+         , connect/1
+         , ping/0
+         , where/0
+         , info/0
+        ]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-				 terminate/2, code_change/3]).
+         terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE). 
+-define(SERVER, ?MODULE).
 
--record(state, {}).
+-record(state, {nodes}).
 
 %%%============================================================================
 %%% API
 %%%============================================================================
 
 start_link() ->
-		gen_server:start_link({global, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link({global, ?SERVER}, ?MODULE, [], []).
 
 attach(Node) ->
-		gen_server:call({global, ?SERVER}, {attach, Node}).
+    gen_server:call({global, ?SERVER}, {attach, Node}).
+
+connect(Node) ->
+    gen_server:call({global, ?SERVER}, {connect, Node}).
 
 ping() ->
-		gen_server:call({global, ?SERVER}, ping).
+    gen_server:call({global, ?SERVER}, ping).
+
+info() ->
+    gen_server:call({global, ?SERVER}, info).
+
+where() ->
+    gen_server:call({global, ?SERVER}, where).
 
 %%%============================================================================
 %%% gen_server callbacks
 %%%============================================================================
 
 init([]) ->
-		slave_node:start_link(node()),
-		{ok, #state{}}.
+    catch	slave_node:start_link(node()),
+    printer:format("New master node: ~p~n", [node()]),
+    {ok, #state{nodes = [node()]}}.
 
 
 
-handle_call({attach, Node}, _From, State) ->
-		case net_adm:ping(Node) of
-				pong -> MasterNode = node(),
-								spawn(Node, slave_node, start_link, [MasterNode]),
-								{reply, ok, State};
-				pang -> {reply, can_not_connect, State}
-		end;
+handle_call({attach, Node}, _From, #state{nodes=Nodes} = State) ->
+    case net_adm:ping(Node) of
+        pong -> spawn(Node, slave_node, start_link, [node()]),
+                {reply, ok, State#state{nodes = [Node|Nodes]}};
+        pang -> {reply, can_not_connect, State}
+    end;
+
+handle_call({connect, Node}, _From, #state{nodes=Nodes} = State) ->
+    printer:format("Connect: ~p~n", [Node]),
+    {reply, ok, State#state{nodes = [Node|Nodes]}};
 
 handle_call(ping, _From, State) -> {reply, pong, State};
+handle_call(where, _From, State) -> {reply, node(), State};
+handle_call(info, _From, State) -> {reply, State, State};
 
-handle_call(_Request, _From, State) ->
-		Reply = ok,
-		{reply, Reply, State}.
+handle_call(Request, _From, State) ->
+    printer:format("Request: ~p~n", [Request]),
+    Reply = ok,
+    {reply, Reply, State}.
 
 
 
 handle_cast(_Msg, State) ->
-		{noreply, State}.
+    {noreply, State}.
 
 
 
 handle_info(_Info, State) ->
-		{noreply, State}.
+    {noreply, State}.
 
 
 
 terminate(_Reason, _State) ->
-		ok.
+    ok.
 
 
 
 code_change(_OldVsn, State, _Extra) ->
-		{ok, State}.
+    {ok, State}.
 
 %%%============================================================================
 %%% Internal functions
--- slave_node.erl
+++ slave_node.erl
@@ -12,16 +12,17 @@
 
 %% API
 -export([
-				 start_link/0
-				 , start_link/1
-				 , ping/0
-				]).
+         start_link/0
+         , start_link/1
+         , ping/0
+         , connect/1
+        ]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-				 terminate/2, code_change/3]).
+         terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE). 
+-define(SERVER, ?MODULE).
 
 -record(state, {}).
 
@@ -30,63 +31,80 @@
 %%%============================================================================
 
 start_link() ->
-		gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 start_link(MasterNode) ->
-		gen_server:start_link({local, ?SERVER}, ?MODULE, [MasterNode], []).
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [MasterNode], []).
 
 ping() ->
-		gen_server:call(?SERVER, ping).
+    gen_server:call(?SERVER, ping).
+
+connect(Node) ->
+    gen_server:call(?SERVER, {connect, Node}).
 
 %%%============================================================================
 %%% gen_server callbacks
 %%%============================================================================
 
 init([]) ->
-		random:seed(now()),
-		{ok, #state{}};
+    random:seed(now()),
+    {ok, #state{}};
 
 init([MasterNode]) ->
-		monitor_node(MasterNode, true),
-		printer:format("init slave_node~p~n", [MasterNode]),
-		{ok, #state{}}.
+    monitor_node(MasterNode, true),
+    printer:format("Init slave_node. Master node: ~p~n", [MasterNode]),
+    {ok, #state{}}.
 
 
 
 handle_call(ping, _From, State) ->
-		printer:format("ping slave_node~n"),
-		{reply, pong, State};
+    printer:format("ping slave_node~n"),
+    {reply, pong, State};
+
+handle_call({connect, Node}, _From, State) ->
+    case net_adm:ping(Node) of
+        pong -> master_node:connect(node()),
+                {reply, ok, State};
+
+        pang -> {reply, {error, pang}, State}
+    end;
 
 handle_call(_Request, _From, State) ->
-		Reply = ok,
-		{reply, Reply, State}.
+    Reply = ok,
+    {reply, Reply, State}.
 
 
 
 handle_cast(_Msg, State) ->
-		{noreply, State}.
+    {noreply, State}.
 
 
 
 handle_info({nodedown, Node}, State) ->
-		timer:sleep(random:uniform(50)),
-		catch master_node:start_link(),
-		printer:format("Node down: ~p~n", [Node]),
-		{noreply, State};
+    timer:sleep(random:uniform(50)),
+    try {ok, _} = master_node:start_link()
+    catch _:_ -> timer:sleep(50),
+                 MasterNode = master_node:where(),
+                 monitor_node(MasterNode, true),
+                 printer:format("Set new master node: ~p~n", [MasterNode]),
+                 master_node:connect(node())
+    end,
+    printer:format("Node down: ~p~n", [Node]),
+    {noreply, State};
 
 handle_info(Info, State) ->
-		printer:format("Info: ~p~n", [Info]),
-		{noreply, State}.
+    printer:format("Info: ~p~n", [Info]),
+    {noreply, State}.
 
 
 
 terminate(_Reason, _State) ->
-		ok.
+    ok.
 
 
 
 code_change(_OldVsn, State, _Extra) ->
-		{ok, State}.
+    {ok, State}.
 
 %%%============================================================================
 %%% Internal functions
