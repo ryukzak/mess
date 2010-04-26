d22cf3009c0010d2b9b9aa469e05e9c721cf77c6
Ryukzak Neskazov <ryukzak@ryukzak-laptop.(none)>
2010-04-27 03:36:14 +0400

    * src/test_util.erl: refactoring

--- src/test_util.erl
+++ src/test_util.erl
@@ -21,26 +21,25 @@
 upload(Node) ->
     {ok,FileList} = file:list_dir(?EBIN),
 
-    Apps = [list_to_atom(re:replace(N, ".app","", [{return,list}]))
-            || N <- FileList,
-               case re:run(N, ".app") of
-                   {match, _} -> true;
-                   _ -> false
-               end], %fixme regexp
-
-    Modules = [list_to_atom(re:replace(N, ".beam","", [{return,list}]))
-               || N <- FileList,
-                  case re:run(N, ".beam") of
-                      {match, _} -> true;
-                      _ -> false
-                  end], %fixme regexp
+    Apps = get_atom_from_file_list(".app", FileList),
+    Modules = get_atom_from_file_list(".beam", FileList),
 
     [send_app(Node, A) || A <- Apps],
-    [send_module(Node, M) || M <- Modules].
+    [send_module(Node, M) || M <- Modules],
+    ok.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+get_atom_from_file_list(Ext0, FileList) ->
+    Ext1 = Ext0++[$$],
+    [list_to_atom(re:replace(N, Ext1,"", [{return,list}]))
+     || N <- FileList,
+        case re:run(N, Ext1) of
+            {match, _} -> true;
+            _ -> false
+        end].
 
 send_app(Node, App) ->
     {ok, AppSpec} = file:script(?EBIN ++ atom_to_list(App) ++ ".app"),
