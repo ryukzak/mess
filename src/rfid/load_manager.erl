3d293173fbcb6c7b9d1a52566bc375f9d6377635
Ryukzak Neskazov <ryukzak@ryukzak-laptop.(none)>
2010-05-05 00:22:16 +0400

    Added new module for nonblock_tcp_accept. Added new load manager. It will be using for distribute load on cluster.

---
+++ src/rfid/load_manager.erl
@@ -0,0 +1,140 @@
+%%%-------------------------------------------------------------------
+%%% @author Ryukzak Neskazov <>
+%%% @copyright (C) 2010, Ryukzak Neskazov
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25 Apr 2010 by Ryukzak Neskazov <>
+%%%-------------------------------------------------------------------
+-module(load_manager).
+
+-behaviour(gen_server).
+
+%% API
+-export([
+				 start_link/1
+				]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+				 terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(state, {}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link(Port) ->
+		gen_server:start_link({local, ?SERVER}, ?MODULE, {Port}, []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initiates the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init({Port}) ->
+		{ok, _Pid} = nonblock_tcp_accept:start_link(self(),
+                                                Port,
+                                                [list, {active, false}]),
+		error_logger:info_msg("load_manager start on port: ~p~n", [Port]),
+		{ok, #state{}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+		Reply = ok,
+		{reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+		{noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info({tcp_accept, Sock}, State) ->
+    IP = "127.0.0.1", % master_node:get_node_ip(),
+		gen_tcp:send(Sock, IP ++ "\n"),
+		gen_tcp:close(Sock),
+		{noreply, State};
+
+handle_info(Info, State) ->
+		error_logger:warning_msg("Load manager, unknown message: ~p", [Info]),
+		{noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+		ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+		{ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
